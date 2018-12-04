716501fdfc00c301cf615f0e534a2108d597ecf4
Tricktionary <andyaoux@hotmail.com>
2018-12-03 21:20:42 -0500

    Done day2 part2

--- day2/day2.rb
+++ day2/day2.rb
@@ -40,18 +40,25 @@ def closestString()
 
   inputArray.each_with_index do |input1, i|
     inputArray[i + 1..inputArray.size].each do |input2|
-      dif = 0
-      input1.each_char.with_index.count {|x,k|
-        if x != input2.chars[k]
-          dif += 1 
-        end       
-      }
-      if dif == 1
-        puts x
-      end
+      input1Split = input1.split('')
+      input2Split = input2.split('')
       
+      diff = 0 
+      newString = Array.new 
+      for x in 0..input1Split.length
+        if input1Split.at(x) != input2Split.at(x)
+          diff+=1
+        else
+          newString.push(input1Split.at(x))
+        end
+      end
+      if diff == 1
+        puts "#{input1} == #{input2}"
+        puts newString.join
+      end
     end
   end
 end
 
 checkSum()
+closestString()
